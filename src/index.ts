c653712a1b1404122645acaefb8d12fb7c74b5e1
ccrsxx <aminrisal@gmail.com>
2024-07-17 05:09:35 +0700

    feat: finish project

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,6 @@ import { showMenu } from './cli/menu.js';
 import { quitCli } from './utils/helper.js';
 import './utils/event.js';
 
-/**
- * Main function to run the CLI.
- */
 export async function main(): Promise<void> {
   while (true) {
     try {
@@ -21,7 +18,4 @@ export async function main(): Promise<void> {
   await quitCli();
 }
 
-void main();
-
-export { showMenu } from './cli/menu.js';
-export { quitCli } from './utils/helper.js';
+if (require.main === module) void main();
