d09a05710fd3f2187468ed10eb00a3f4a1bf8417
ccrsxx <aminrisal@gmail.com>
2024-07-17 04:59:02 +0700

    feat: adding main import to test

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { showMenu } from './cli/menu.js';
 import { quitCli } from './utils/helper.js';
 import './utils/event.js';
 
-async function main(): Promise<void> {
+export async function main(): Promise<void> {
   while (true) {
     try {
       await showMenu();
