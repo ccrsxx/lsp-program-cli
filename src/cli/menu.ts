929ecc9cc080c97da423293a71881a8679e506ae
ccrsxx <aminrisal@gmail.com>
2024-07-17 05:28:41 +0700

    feat: finish project

--- src/cli/menu.ts
+++ src/cli/menu.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-constant-condition */
+
 import { rawlist } from '@inquirer/prompts';
 import { sortNumbers } from './sort.js';
 import { inputNumbers } from './input.js';
@@ -5,55 +7,61 @@ import { searchNumbers } from './search.js';
 import { showLogo, showLoadingSpinner, quitCli } from '../utils/helper.js';
 
 export async function showMenu(): Promise<void> {
-  await showLogo();
+  while (true) {
+    await showLogo();
 
-  const menu = [
-    'quit_cli',
-    'sort_numbers',
-    'input_numbers',
-    'search_numbers'
-  ] as const;
+    const menu = [
+      'quit_cli',
+      'sort_numbers',
+      'input_numbers',
+      'search_numbers'
+    ] as const;
 
-  type Menu = (typeof menu)[number];
+    type Menu = (typeof menu)[number];
 
-  const answer = await rawlist<Menu>({
-    message: 'Choose Menu',
-    choices: [
-      {
-        name: 'Input Numbers',
-        value: 'input_numbers'
-      },
-      {
-        name: 'Sort Numbers',
-        value: 'sort_numbers'
-      },
-      {
-        name: 'Search Numbers',
-        value: 'search_numbers'
-      },
-      {
-        name: 'Quit',
-        value: 'quit_cli'
+    try {
+      const answer = await rawlist<Menu>({
+        message: 'Choose Menu',
+        choices: [
+          {
+            name: 'Input Numbers',
+            value: 'input_numbers'
+          },
+          {
+            name: 'Sort Numbers',
+            value: 'sort_numbers'
+          },
+          {
+            name: 'Search Numbers',
+            value: 'search_numbers'
+          },
+          {
+            name: 'Quit',
+            value: 'quit_cli'
+          }
+        ]
+      });
+
+      await showLoadingSpinner();
+
+      switch (answer) {
+        case 'input_numbers':
+          await inputNumbers();
+          break;
+        case 'sort_numbers':
+          await sortNumbers();
+          break;
+        case 'search_numbers':
+          await searchNumbers();
+          break;
+        case 'quit_cli':
+          await quitCli();
+          break;
+        default:
+          break;
       }
-    ]
-  });
-
-  await showLoadingSpinner();
-
-  switch (answer) {
-    case 'input_numbers':
-      await inputNumbers();
+    } catch {
       break;
-    case 'sort_numbers':
-      await sortNumbers();
-      break;
-    case 'search_numbers':
-      await searchNumbers();
-      break;
-    case 'quit_cli':
-      await quitCli();
-      break;
-    default:
-      break;
+    }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,11 @@
 #!/usr/bin/env node
 
-/* eslint-disable no-constant-condition */
-
 import { showMenu } from './cli/menu.js';
 import { quitCli } from './utils/helper.js';
 import './utils/event.js';
 
 async function main(): Promise<void> {
-  while (true) {
-    try {
-      await showMenu();
-    } catch {
-      break;
-    }
-  }
-
+  await showMenu();
   await quitCli();
 }
 
